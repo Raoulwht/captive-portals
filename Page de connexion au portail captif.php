fda464e673c7e56bcca6d386289c9f11fd44c3fb
Raoulwht <97730601+Raoulwht@users.noreply.github.com>
2023-01-13 12:43:35 +0100

    first commit

---
+++ Page de connexion au portail captif.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<!-- saved from url=(0099)http://192.168.100.254:8002/index.php?zone=royal_cafe_lounge&redirurl=http%3A%2F%2F8.8.8.8%2F#terms -->
+<html class="translated-ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <title>Page de connexion au portail captif</title>
+  <style>
+	  #content,.login,.login-card a,.login-card h1,.login-help{text-align:center}body,html{margin:0;padding:0;width:100%;height:100%;display:table}#content{font-family:'Source Sans Pro',sans-serif;background-color:#1C1275;background:url(captiveportal-background..jpeg)center center no-repeat fixed;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;display:table-cell;vertical-align:middle}.login-card{padding:40px;width:280px;background-color:#F7F7F7;margin:100px auto 10px;border-radius:2px;box-shadow:0 2px 2px rgba(0,0,0,.3);overflow:hidden}.login-card h1{font-weight:400;font-size:2.3em;color:#1383c6}.login-card h1 span{color:#f26721}.login-card img{width:70%;height:70%}.login-card input[type=submit]{width:100%;display:block;margin-bottom:10px;position:relative}.login-card input[type=text],input[type=password],input[type=number]{height:44px;font-size:16px;width:100%;margin-bottom:10px;-webkit-appearance:none;background:#fff;border:1px solid #d9d9d9;border-top:1px solid silver;padding:0 8px;box-sizing:border-box;-moz-box-sizing:border-box}.login-card input[type=text]:hover,input[type=password]:hover{border:1px solid #b9b9b9;border-top:1px solid #a0a0a0;-moz-box-shadow:inset 0 1px 2px rgba(0,0,0,.1);-webkit-box-shadow:inset 0 1px 2px rgba(0,0,0,.1);box-shadow:inset 0 1px 2px rgba(0,0,0,.1)}.login{font-size:14px;font-family:Arial,sans-serif;font-weight:700;height:36px;padding:0 8px}.login-submit{-webkit-appearance:none;-moz-appearance:none;appearance:none;border:0;color:#fff;text-shadow:0 1px rgba(0,0,0,.1);background-color:#fb2525}.login-submit:disabled{opacity:.6}.login-submit:hover{border:0;text-shadow:0 1px rgba(0,0,0,.3);background-color:#fa5353}.login-card a{text-decoration:none;color:#222;font-weight:400;display:inline-block;opacity:.6;transition:opacity ease .5s}.login-card a:hover{opacity:1}.login-help{width:100%;font-size:12px}.list{list-style-type:none;padding:0}.list__item{margin:0 0 .7rem;padding:0}label{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;text-align:left;font-size:14px;}input[type=checkbox]{-webkit-box-flex:0;-webkit-flex:none;-ms-flex:none;flex:none;margin-right:10px;float:left}@media screen and (max-width:450px){.login-card{width:70%!important}.login-card img{width:30%;height:30%}}textarea{width:66%;margin:auto;height:120px;max-height:120px;background-color:#f7f7f7;padding:20px}#terms{display:none;padding-top:100px;padding-bottom:300px;}.auth_source{border: 1px solid lightgray; padding:20px 8px 0px 8px; margin-top: -2em; border-radius: 2px; }.auth_head{background-color:#f7f7f7;display:inline-block;}.auth_head_div{text-align:left;}#error-message{text-align:left;color:#ff3e3e;font-style:italic;}
+  </style>
+<link type="text/css" rel="stylesheet" charset="UTF-8" href="./Page de connexion au portail captif_files/translateelement.css"></head>
+
+<body>
+<div id="content" style="background: url(captiveportal-Entrance.jpg); background-repeat: no-repeat; background-position: center; background-size: cover;">
+	<div class="login-card">
+		<img src="captiveportal-Logo1-removebg-preview.png" style="height: 15vh; width: 100px;"><br>
+ 		<h1></h1>
+		<div id="error-message">
+			
+		</div>
+	  <form name="login_form" method="post" action="./Second/login.php">		<input type="text" name="auth_user" placeholder="Email(Optional)" id="auth_user">
+		<input type="password" name="auth_pass" placeholder="Mot de passe" id="auth_pass" required>				<br><br>
+				<input name="auth_voucher" type="number" placeholder="Telephone" value="" required>		  <div class="login-help">
+			<ul class="list">
+				<li class="list__item">
+				  <label class="label--checkbox">
+					<input type="checkbox" class="checkbox" onchange="document.getElementById(&#39;login&#39;).disabled = !this.checked;">
+					<span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">je suis d'accord avec les</font></font><a rel="noopener" href="http://192.168.100.254:8002/index.php?zone=royal_cafe_lounge&amp;redirurl=http%3A%2F%2F8.8.8.8%2F#terms" onclick="document.getElementById(&#39;terms&#39;).style.display = &#39;block&#39;;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">termes et conditions</font></font></a></span>
+				  </label>
+				</li>
+			</ul>
+		  </div>
+		<input name="redirurl" type="hidden" value="http://8.8.8.8/">
+		<font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><input type="submit" name="accept" class="login login-submit" value="Connexion" id="login" disabled=""></font></font>
+	  </form>
+	  <br>
+	  <span> <i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> </font></font></i> <strong><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"></font></font></strong></span>
+	</div>
+	<div id="terms">
+		<textarea readonly="">Generic Terms and Conditions Template
+Please read these terms and conditions ("terms and conditions", "terms") carefully before using 
+[website] website (“website”, "service") operated by [name] ("us", 'we", "our").
+Conditions of use
+By using this website, you certify that you have read and reviewed this Agreement and that you 
+agree to comply with its terms. If you do not want to be bound by the terms of this Agreement, 
+you are advised to leave the website accordingly. [name] only grants use and access of this 
+website, its products, and its services to those who have accepted its terms.
+Privacy policy
+Before you continue using our website, we advise you to read our privacy policy [link to privacy 
+policy] regarding our user data collection. It will help you better understand our practices.
+Age restriction
+You must be at least 18 (eighteen) years of age before you can use this website. By using this 
+website, you warrant that you are at least 18 years of age and you may legally adhere to this 
+Agreement. [name] assumes no responsibility for liabilities related to age misrepresentation.
+Intellectual property
+You agree that all materials, products, and services provided on this website are the property of 
+[name], its affiliates, directors, officers, employees, agents, suppliers, or licensors including all 
+copyrights, trade secrets, trademarks, patents, and other intellectual property. You also agree 
+that you will not reproduce or redistribute the [name]’s intellectual property in any way, 
+including electronic, digital, or new trademark registrations.
+You grant [name] a royalty-free and non-exclusive license to display, use, copy, transmit, and 
+broadcast the content you upload and publish. For issues regarding intellectual property claims, 
+you should contact the company in order to come to an agreement.
+User accounts
+As a user of this website, you may be asked to register with us and provide private information. 
+You are responsible for ensuring the accuracy of this information, and you are responsible for 
+maintaining the safety and security of your identifying information. You are also responsible for 
+all activities that occur under your account or password.
+If you think there are any possible issues regarding the security of your account on the website, 
+inform us immediately so we may address them accordingly.
+We reserve all rights to terminate accounts, edit or remove content and cancel orders at our sole 
+discretion.
+Terms and conditions template by WebsitePolicies.com
+Applicable law
+By visiting this website, you agree that the laws of the [location], without regard to principles of 
+conflict laws, will govern these terms and conditions, or any dispute of any sort that might come 
+between [name] and you, or its business partners and associates.
+Disputes
+Any dispute related in any way to your visit to this website or to products you purchase from us 
+shall be arbitrated by state or federal court [location] and you consent to exclusive jurisdiction 
+and venue of such courts.
+Indemnification
+You agree to indemnify [name] and its affiliates and hold [name] harmless against legal claims 
+and demands that may arise from your use or misuse of our services. We reserve the right to 
+select our own legal counsel. 
+Limitation on liability
+[name] is not liable for any damages that may occur to you as a result of your misuse of our 
+website.
+[name] reserves the right to edit, modify, and change this Agreement at any time. We shall let 
+our users know of these changes through electronic mail. This Agreement is an understanding 
+between [name] and the user, and this supersedes and replaces all prior agreements regarding 
+the use of this website.</textarea>
+	</div>
+</div>
+
+
